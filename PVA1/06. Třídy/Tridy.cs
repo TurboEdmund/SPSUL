b83c14fd38e2c7658331118ae5ca43b5a9d14edc
Marek Helis <marek.helis@goodaccess.com>
2025-01-10 23:32:18 +0100

    Tridy update

---
+++ PVA1/06. Třídy/Tridy.cs
@@ -0,0 +1,42 @@
+﻿namespace Tridy
+{
+    public class Person
+    {
+        public readonly string FirstName; // proměnná readonly lze pouze nastavit v konstruktoru, pak ji nelze změnit
+        public string LastName; // public proměnné je PascalCase
+        private int _age; // privátní proménně se píšou _camelCase
+        private double _weight;
+
+        public Person(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this._age = 0;
+        }
+        public Person(string firstName, string lastName, double weight)
+            : this(firstName, lastName)
+        {
+            this._weight = weight;
+        }
+
+        public void IntroduceYourself()
+        {
+            Console.WriteLine($"Hello, I am {this.FirstName} {this.LastName} and I am {this._age} years old and weigh {this._weight}kg.");
+        }
+
+        private void Walk()
+        {
+            // ... some code
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Jakub", "Doležal", 5);
+            p.IntroduceYourself();
+        }
+    }
+}
+
+
