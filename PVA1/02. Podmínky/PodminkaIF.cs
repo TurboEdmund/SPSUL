b5b49b1f8b72c9ebac9a1640c61faece359e36ba
Marek Helis <marek.helis@goodaccess.com>
2025-01-11 11:55:33 +0100

    Přidání ukázek k podmínkám

---
+++ PVA1/02. Podmínky/PodminkaIF.cs
@@ -0,0 +1,22 @@
+﻿namespace Ukazka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej číslo: ");
+            int cislo = int.Parse(Console.ReadLine()); // uživatel zadá číslo
+
+            if (cislo > 10) // cislo je v intervalu (∞, 10)
+            {
+                Console.WriteLine("Číslo je větší než 10");
+            } else if (cislo > 5) // cislo je v intervalu (10, 5)
+            {
+                Console.WriteLine("Číslo je menší nebo rovno 10 a větší než 5");
+            } else // cislo je v intervalu <5, -∞)
+            {
+                Console.WriteLine("Číslo je menší nebo rovno 5");
+            }
+        }
+    }
+}
---
+++ PVA1/02. Podmínky/Switch.cs
@@ -0,0 +1,31 @@
+﻿namespace Ukazka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej hlavní město ČR: ");
+            string mesto = Console.ReadLine(); // uživatel zadá město
+
+            switch (mesto) // do switche vstupuje hodnota, která je následně porovnáváná - switch funguje na bázi rovnosti (==)
+            {
+                case "Praha":
+                    Console.WriteLine("Správná odpověď!");
+                    break;
+                case "Brno":
+                case "Ostrava":
+                case "Plzeň":
+                    Console.WriteLine("Jsi blízko, toto město je velké, ale není hlavním městem ČR");
+                    // jelikož u Brna, Ostavy a Plzně bude stejná odpověď, napíšeme je za sebe a dáme jim stejnou odpověď - nebudeme zbytečně psát pro každé město svůj vlastní case, odpověď a break
+                    break;
+                case "Ústí nad Labem":
+                    Console.WriteLine("To si děláš srandu ne? tohle určitě není hlavní město");
+                    break;
+                default:
+                    Console.WriteLine("Špatná odpověď");
+                    // default je takový ELSE ve switche, pokud žádná z možností není splněna, vykoná se default
+                    break;
+            }
+        }
+    }
+}
