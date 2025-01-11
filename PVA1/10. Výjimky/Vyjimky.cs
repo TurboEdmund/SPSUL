5df5dc5379ebaf92f4eb326747f67b734af818e2
Marek Helis <marek.helis@goodaccess.com>
2025-01-11 16:03:27 +0100

    Update vyjimky

---
+++ PVA1/10. Výjimky/Vyjimky.cs
@@ -0,0 +1,42 @@
+﻿namespace Ukazka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej číslo: ");
+            try
+            {
+                int x = int.Parse(Console.ReadLine()); // pokud uživatel nezadá číslo, tak se vyvolá FormatException
+                Console.WriteLine($"Zadal jsi číslo {x}"); // pokud řáde nad vyvolá výjimku, tento řádek se už neprovede
+            }
+            catch
+            {
+                Console.WriteLine("Nezadal jsi číslo!"); // tento řádek se provede pouze pokud uživatel nezadá číslo
+            }
+            finally
+            {
+                // Tento blok se provede vždy, i pokud došlo k výjimce
+                Console.WriteLine("Děkujeme za použití programu.");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Zadej číslo: ");
+            try
+            {
+                int x = int.Parse(Console.ReadLine()); // pokud uživatel nezadá číslo, tak se vyvolá FormatException
+                Console.WriteLine($"Zadal jsi číslo {x}"); // pokud řáde nad vyvolá výjimku, tento řádek se už neprovede
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Program selhal z důvodu: {e.Message}"); // můžeme si vypsat důvod, proč program selhal
+            }
+            finally
+            {
+                // Tento blok se provede vždy, i pokud došlo k výjimce
+                Console.WriteLine("Děkujeme za použití programu.");
+            }
+        }
+    }
+}
