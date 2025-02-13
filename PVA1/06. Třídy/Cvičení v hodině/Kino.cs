06865fe513c0064ad4d469d5162af1307e988ea2
Marek Helis <marek.helis@goodaccess.com>
2025-02-13 10:57:28 +0100

    Tridy a prace v hodině

---
+++ PVA1/06. Třídy/Cvičení v hodině/Kino.cs
@@ -0,0 +1,137 @@
+﻿namespace Film
+{
+    public class Kino
+    {
+        private List<Film> filmy;
+
+        public Kino(List<Film> filmy)
+        {
+            this.filmy = filmy;
+        }
+
+        public void ZobrazNabidku()
+        {
+            Console.WriteLine("Filmy:");
+            foreach (Film film in filmy)
+            {
+                Console.WriteLine($"- {film.Name}, {film.Delka}h");
+                Console.WriteLine($"-- Reziser: {film.Reziser.FirstName} {film.Reziser.LastName}");
+                Console.WriteLine($"-- Zanry: {string.Join(", ", film.Zanry)}");
+                Console.WriteLine("-- Herci:");
+                foreach(Clovek herec in film.Herci)
+                {
+                    Console.WriteLine($"--- {herec.FirstName} {herec.LastName} {herec.Age}");
+                }
+            }
+        }
+    }
+    public class Film
+    {
+        public string Name;
+        public List<Clovek> Herci;
+        public Clovek Reziser;
+        public double Delka;
+        public List<Zanr> Zanry;
+
+        public Film(string name, List<Clovek> herci,Clovek reziser, double delka, List<Zanr> zanry)
+        {
+            this.Name = name;
+            this.Herci = herci;
+            this.Reziser = reziser;
+            this.Delka = delka;
+            this.Zanry = zanry;
+        }
+    }
+    public class Clovek
+    {
+        public string FirstName;
+        public string LastName;
+        public int Age;
+
+        public Clovek(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+    public enum Zanr
+    {
+        Action,
+        Comedy,
+        Horror,
+        Thriller,
+        SciFi
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kino kino = new Kino(
+                new List<Film>()
+                {
+                    new Film(
+                        "Top Gun: Maverick",
+                        new List<Clovek>()
+                        {
+                            new Clovek(
+                                "Tom",
+                                "Cruise",
+                                62
+                            ),
+                            new Clovek(
+                                "Miles",
+                                "Teller",
+                                37
+                            )
+                        },
+                        new Clovek(
+                            "Joseph",
+                            "Kosinski",
+                            50
+                        ),
+                        2.5,
+                        new List<Zanr>()
+                        {
+                            Zanr.Action,
+                            Zanr.Comedy
+                        }
+                    ),
+                    new Film(
+                        "Avatar: The Way of Water",
+                        new List<Clovek>()
+                        {
+                            new Clovek(
+                                "Sam",
+                                "Worthington",
+                                48
+                            ),
+                            new Clovek(
+                                "Zoe",
+                                "Saldana",
+                                46
+                            ),
+                            new Clovek(
+                                "Sigourney",
+                                "Weaver",
+                                75
+                            )
+                        },
+                        new Clovek(
+                            "James",
+                            "Cameron",
+                            70
+                        ),
+                        3.2,
+                        new List<Zanr>()
+                        {
+                            Zanr.Action,
+                            Zanr.SciFi
+                        }
+                    )
+                }
+            );
+            kino.ZobrazNabidku();
+        }
+    }
+}
---
+++ PVA1/06. Třídy/Cvičení v hodině/ObchodHudba.cs
@@ -0,0 +1,98 @@
+﻿namespace Film
+{
+    public class Obchod
+    {
+        private List<Album> _nabidka;
+
+        public Obchod(List<Album> nabidka)
+        {
+            this._nabidka = nabidka;
+        }
+
+        public void ZobrazNabidku()
+        {
+            Console.WriteLine("Nabidka:");
+            foreach (Album album in _nabidka)
+            {
+                Console.WriteLine($"- {album.Interpret.Nazev} - {album.Name} - {album.Zanr}");
+                foreach(Skladba skladba in album.Skladby)
+                {
+                    Console.WriteLine($"-- {skladba.Name} ({skladba.Delka}m)");
+                }
+            }
+        }
+    }
+    public enum Zanr
+    {
+        Pop,
+        Rock,
+        SynthPop
+    }
+    public class Album
+    {
+        public string Name;
+        public Interpret Interpret;
+        public List<Skladba> Skladby;
+        public Zanr Zanr;
+
+        public Album(string name, Interpret interpret, List<Skladba> skladby, Zanr zanr)
+        {
+            Name = name;
+            Interpret = interpret;
+            Skladby = skladby;
+            Zanr = zanr;
+        }
+    }
+    public class Interpret
+    {
+        public string Nazev;
+
+        public Interpret(string nazev)
+        {
+            this.Nazev = nazev;
+        }
+    } 
+    public class Skladba
+    {
+        public string Name;
+        public double Delka;
+
+        public Skladba(string name, double delka)
+        {
+            Name = name;
+            Delka = delka;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Obchod o = new Obchod(
+                new List<Album>()
+                {
+                    new Album(
+                        "Memento Mori",
+                        new Interpret("Depeche Mode"),
+                        new List<Skladba>()
+                        {
+                            new Skladba("My Cosmos Is Mine", 5.16),
+                            new Skladba("Ghosts Again", 3.9)
+                        },
+                        Zanr.SynthPop
+                    ),
+                    new Album(
+                        "Back In Black",
+                        new Interpret("AC/DC"),
+                        new List<Skladba>()
+                        {
+                            new Skladba("Hells Bells", 5.1),
+                            new Skladba("Back In Black", 4.2)
+                        },
+                        Zanr.Rock
+                    )
+                } 
+            );
+            o.ZobrazNabidku();
+        }
+    }
+}
